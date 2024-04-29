852bcc3b01191fd6560ecf6413cf036139201eb4
Marco Lam <marcolam1111@gmail.com>
2024-04-29 09:51:05 -0400

    Add test for fetchEventsForDay

--- bu_passport/test/firebase_service_test.dart
+++ bu_passport/test/firebase_service_test.dart
@@ -17,6 +17,7 @@ void main() {
       fakeFirebaseFirestore = FakeFirebaseFirestore();
     });
 
+    // Testing for fetchEvents in firebase_service
     test('fetchEvents gets data from a given collection', () async {
       final FirebaseService firebaseService =
           FirebaseService(db: fakeFirebaseFirestore!);
@@ -46,6 +47,8 @@ void main() {
       print(eventList[0].eventID);
       expect(eventList.length, 1); // Make sure only one event is fetched
     });
+
+    // Testing for fetchUser in firebase_service
     test('fetchUser gets data from a given collection', () async {
       final FirebaseService firebaseService =
           FirebaseService(db: fakeFirebaseFirestore!);
@@ -74,6 +77,65 @@ void main() {
 
       final Users? fetchedUser = await firebaseService.fetchUser('user1');
       expect(fetchedUser?.userUID, 'user1');
+    });
+
+    test('fetchEventsForDay gets data from a given collection', () async {
+      final FirebaseService firebaseService =
+          FirebaseService(db: fakeFirebaseFirestore!);
+
+      const String collectionPath = 'events';
+      const String documentId1 = 'event1';
+      const String documentId2 = 'event2';
+
+      Map<String, dynamic> data1 = {
+        "eventID": "1",
+        "eventTitle": "Event on Target Date",
+        "eventPhoto": "assets/images/arts/image9.jpeg",
+        "eventLocation": "Test Location",
+        "eventStartTime": DateTime(2024, 4, 29, 10, 0),
+        "eventEndTime": DateTime(2024, 4, 29, 12, 0),
+        "eventDescription": "Test Description",
+        "eventPoints": 30,
+        "eventURL": "http://example.com",
+        "savedUsers": []
+      };
+
+      Map<String, dynamic> data2 = {
+        "eventID": "2",
+        "eventTitle": "Event Not on Target Date",
+        "eventPhoto": "assets/images/arts/image9.jpeg",
+        "eventLocation": "Test Location",
+        "eventStartTime": DateTime(2024, 4, 28, 10, 0),
+        "eventEndTime": DateTime(2024, 4, 28, 12, 0),
+        "eventDescription": "Test Description",
+        "eventPoints": 30,
+        "eventURL": "http://example.com",
+        "savedUsers": []
+      };
+
+      final targetDate = DateTime(2024, 4, 29);
+
+      await fakeFirebaseFirestore!
+          .collection(collectionPath)
+          .doc(documentId1)
+          .set(data1);
+      await fakeFirebaseFirestore!
+          .collection(collectionPath)
+          .doc(documentId2)
+          .set(data2);
+
+      List<Event> allEvents = await firebaseService.fetchEvents();
+
+      final filteredEvents =
+          firebaseService.fetchEventsForDay(targetDate, allEvents);
+
+      print(allEvents[0].eventID);
+      print(allEvents[1].eventID);
+
+      expect(filteredEvents.length, 1); // Should only contain one event
+      expect(filteredEvents[0].eventID, "event1");
+      expect(filteredEvents[0].eventTitle, "Event on Target Date");
+      expect(filteredEvents[0].eventStartTime, DateTime(2024, 4, 29, 10, 0));
     });
   });
 }
